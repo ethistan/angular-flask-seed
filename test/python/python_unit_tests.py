56ceda11c4e892e248deeb1ad8fdb82da5a12928
Liviu Constantinescu <l.constantinescu@garvan.org.au>
2014-10-25 12:04:07 +1100

    Cleanup

--- test/python/python_unit_tests.py
+++ test/python/python_unit_tests.py
@@ -6,13 +6,14 @@ environment = os.environ.get("ENV", "dev")
 
 
 class ServerTestCase(unittest.TestCase):
-	psql_db = None
+    psql_db = None
 
-	def setUp(self):
-		self.app = server.app.test_client()
-		server.init_db("Test")
-		# server.psql_db.clean_test_database()
+    def setUp(self):
+        self.app = server.app.test_client()
+        server.init_db("Test")
 
-	def test_index_page(self):
-		rv = self.app.get('/')
-		self.assertTrue(rv.data.find("CCG Sample App"))
+    # server.psql_db.clean_test_database()
+
+    def test_index_page(self):
+        rv = self.app.get('/')
+        self.assertTrue(rv.data.find("CCG Sample App"))
