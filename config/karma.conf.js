17363850573828f70d653a9124123d01638c200e
Peter Budd <ethistan@gmail.com>
2015-05-06 15:43:30 +1000

    ALF-442 Update Seed project to use latest Alfred deployment strategies
    Fixed some merging issues

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,24 +2,6 @@ module.exports = function (config) {
 	config.set({
 		basePath: '..',
 
-<<<<<<< Updated upstream:config/dev/karma.conf.js
-        files: [
-            'static/lib/angular/angular.js',
-            'static/lib/angular/angular-*.js',
-            'test/lib/angular/angular-mocks.js',
-            'static/lib/angular/angular-route.js',
-            'static/lib/angular/angular-sanitize.js',
-            'static/lib/angular/angular-animate.js',
-            'static/lib/jQuery/jquery-1.11.1.js',
-            'static/lib/bootstrap/bootstrap.min.js',
-            'static/js/**/*.js',
-            'test/unit/**/*.js'
-        ],
-
-        preprocessors: {
-            'static/js/**/*.js': 'coverage'
-        },
-=======
 		files: [
 			'test/lib/jQuery/jquery-1.10.2.js',
 			'static/lib/angular/angular.js',
@@ -27,7 +9,6 @@ module.exports = function (config) {
 			'static/js/**/*.js',
 			'test/unit/**/*.js'
 		],
->>>>>>> Stashed changes:config/karma.conf.js
 
 		exclude: [
 			'static/lib/angular/*.min.js',
@@ -41,18 +22,6 @@ module.exports = function (config) {
 
 		browsers: ['PhantomJS'],
 
-<<<<<<< Updated upstream:config/dev/karma.conf.js
-        reporters: ['progress', 'junit', 'coverage'],
-
-		plugins: [
-            'karma-junit-reporter',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-coverage'
-		]
-=======
 		plugins: [
 			'karma-junit-reporter',
 			'karma-chrome-launcher',
@@ -77,6 +46,5 @@ module.exports = function (config) {
 			type: 'html',
 			dir: 'test_out/coverage/'
 		}
->>>>>>> Stashed changes:config/karma.conf.js
 	})
 };
